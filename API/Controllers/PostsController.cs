dffe5da44ab2a7657354cbee7ecacb88fa7f5d29
Juliana Cortes <cortej19@matc.edu>
2024-10-31 14:01:45 -0400

    Fix

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
 using Domain;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
 
@@ -20,5 +21,61 @@ namespace API.Controllers
         {
             return this._context.Posts.ToList();
         }
+        //Get api/post/[id]
+        [HttpGet("{id}" , Name="GetById")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            var post = this._context.Posts.Find(id);
+            if (post is null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpPost(Name = "Create")]
+        public ActionResult<Post> Create([FromBody] Post request)
+        {
+            var post = new Post
+            {
+                Id= request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+            _context.Posts.Add(post);
+            var success = _context.SaveChanges() >0;
+
+            if(success)
+            {
+                return Ok(post);
+        
+            }
+            throw new Exception("Error creating post");
+        }
+
+        [HttpPut(Name ="Update")]
+
+        public ActionResult<Post> Update([FromBody]Post request)
+        {
+            //Find existing post
+            var post = _context.Posts.Find(request.Id);
+            if(post == null)
+            {
+                throw new Exception("Post not found");
+            }
+            // Updating post properties
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != DateTime.MinValue ? request.Date : post.Date;
+
+            var success = _context.SaveChanges() >0;
+            if(success)
+            {
+                return Ok(post);
+            }
+            throw new Exception("Error updating post");
+        }
+
     }
 }
